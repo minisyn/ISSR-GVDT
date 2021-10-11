bcc8af01b40d4da27ca16e8840e6118f5cc3cf34
d.lozanop <d.lozanop@alumnos.upm.es>
2021-10-11 22:10:36 +0200

    Error1_P2

--- P2/GVDT_Agente2.cs
+++ P2/GVDT_Agente2.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Hola don pepito
-//hola don jose
+
+
 public class GVDT_Agente2 : ISSR_Agent
 {
-    public bool target_fixed = false;
-    // Start is called before the first frame update
     public override void Start()
     {
         //  Debug.LogFormat("{0}: Comienza", Myself.Name);
@@ -19,17 +17,25 @@ public class GVDT_Agente2 : ISSR_Agent
         {
             yield return new WaitForSeconds(0.5f);
             //En este punto generaremos un evento de tiempo
-            //current_event = ISSREventType.onTickElapsed;
+            current_event = ISSREventType.onTickElapsed;
+            current_state = AgentStateMachine();
         }
     }
     ISSRState AgentStateMachine() // Función principal de máquina de estados
     {
         ISSRState next_state = current_state; // estado de salida, en principio igual
 
-
         switch (current_state) // Según el estado
         {
-
+            case ISSRState.Idle:
+                next_state = SF_Idle;
+                break;
+            case ISSRState.GoingToGripSmallStone:
+                next_state = SF_GoingToGripSmallStone();
+                break;
+            case ISSRState.GoingToGoalWithSmallStone:
+                next_state = SF_GoingToGoalWithSmallStone();
+                break;
             default:
                 Debug.LogErrorFormat("{0}: estado {1} no considerado", Myself.Name,
                     current_state);
@@ -52,6 +58,28 @@ public class GVDT_Agente2 : ISSR_Agent
 
         switch (current_event) // Según el evento
         {
+            case ISSREventType.onTickElapsed:
+                //coger de la lista el objeto más cercano al agente del tipo SmallStone (Objeto focuseado)
+                focus_object = ISSRHelp.GetCloserToMeObjectInList(this, Valid_Small_Stones, ISSR_Type.SmallStone);
+
+                if (focus_object != null)
+                {
+                    Debug.LogFormat("{0}: trata de coger {1}", Myself.Name, focus_object.Name);
+                    acGripObject(focus_object);
+                    if (acCheckError())
+                    {
+                        next_state = ISSRState.Error;
+                    }
+                    else
+                    {
+                        next_state = ISSRState.GoingToGripSmallStone;
+                    }
+                }
+                else
+                {
+                    Debug.LogFormat("{0}: no reconozco más piedras pequeñas", Myself.Name);
+                    next_state = ISSRState.End;
+                }
             default:
                 // Debug.LogWarningFormat("{0} Evento '{1}' no considerado en estado '{2}'",                     
                 //    Myself.Name, current_event, current_state);
@@ -66,6 +94,19 @@ public class GVDT_Agente2 : ISSR_Agent
         ISSRState next_state = current_state;
         switch (current_event)
         {
+            case ISSREventType.onGripSuccess:
+                Valid_Small_Stones.Remove(focus_object);
+                acGotoLocation();
+                if (acCheckError)
+                {
+                    next_state = ISSRState.Error;
+                }
+                else
+                {
+                    Debug.Debug.LogFormat("{0}: De camino a la meta", Myself.Name);
+                    next_state = ISSRState.GoingToGoal;
+                }
+                break;
             default:
                 break;
         }
@@ -76,6 +117,9 @@ public class GVDT_Agente2 : ISSR_Agent
         ISSRState next_state = current_state;
         switch (current_event)
         {
+            case ISSREventType.onGObjectScored:
+                next_state = ISSRState.Idle;
+                Debug.LogFormat("{0}: Piedra {1} anotada", Myself.Name, focus_object.Name);
             default:
                 break;
         }
@@ -92,19 +136,21 @@ public class GVDT_Agente2 : ISSR_Agent
             Valid_Small_Stonees.add(obj);
             Debug.LogFormat("{0}: nueva piedra pequeña {1}, anotada", Myself.Name, obj.Name);
         }
+        current_state = AgentStateMachine();
     }
 
     //Podemos programar las acciones
     public override void onGripSuccess(ISSR_Object obj_gripped)
     {
 
-        Debug.LogFormat("{0}: piedra {1} cogida, voy para la meta", Myself.Name, obj_gripped.Name);
-        acGotoLocation(iMyGoalLocation());
+        Debug.LogFormat("{0}: agarra {1}, va para la meta", Myself.Name, obj_gripped.Name);
+        current_state = AgentStateMachine();
     }
 
     public override void onGObjectScored(ISSR_Object stone_that_scored)
     {
         //target_fixed = false;
         Debug.LogFormat("{0}: piedra {1} en meta", Myself.Name, stone_that_scored.Name);
+        current_state = AgentStateMachine();
     }
 }
